33b3d515e576c0d2d69b5b15e40cd57f28990eff
Odai Nasser <odai.karajah90@gmail.com>
2024-05-18 18:25:35 +0400

    notifications an calendar modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
 import { UsersModule } from './modules/security/users/users.module';
 import { AuthModule } from './modules/security/auth/auth.module';
+import { CalendarModule } from './modules/common/calendar/calendar.module';
+import { NotificationsModule } from './modules/common/notifications/notifications.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     UsersModule,
     AuthModule,
+    CalendarModule,
+    NotificationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
