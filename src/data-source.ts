baef07df0d0ce5a5cc5b9485e4900f5bc17a31d1
Odai Nasser <odai.karajah90@gmail.com>
2024-06-02 16:56:51 +0400

    fix typeorm config

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,24 +1,7 @@
 import { DataSource } from 'typeorm';
-import dotenv from 'dotenv';
-import path from 'path';
+import { configService } from './config/config.service';
 
-dotenv.config();
-
-const sslConfig =
-  process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false;
-
-const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  ssl: sslConfig,
-  entities: [path.join(__dirname, '**/*.entity.{ts,js}')],
-  migrations: [path.join(__dirname, 'db/migration/**/*.{ts,js}')],
-  synchronize: false,
-});
+const AppDataSource = new DataSource(configService.getTypeOrmConfig());
 
 AppDataSource.initialize()
   .then(() => {
@@ -28,4 +11,4 @@ AppDataSource.initialize()
     console.error('Error during Data Source initialization:', err);
   });
 
-module.exports = AppDataSource;
+export default AppDataSource;
