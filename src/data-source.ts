4ab4d175fb9d3667f66162ee2016bc9faca938d0
Odai Nasser <odai.karajah90@gmail.com>
2024-06-02 16:03:43 +0400

    orm fixes

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,18 +1,22 @@
-const { DataSource } = require('typeorm');
-const dotenv = require('dotenv');
-const path = require('path');
+import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
+
+const sslConfig =
+  process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false;
 
 const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  ssl: sslConfig,
   entities: [path.join(__dirname, '**/*.entity.{ts,js}')],
-  migrations: [path.join(__dirname, 'migration/**/*.{ts,js}')],
+  migrations: [path.join(__dirname, 'db/migration/**/*.{ts,js}')],
   synchronize: false,
 });
 
