2bc19f0b399bf39082b11e14ba3586d28a88da17
Odai Nasser <odai.karajah90@gmail.com>
2024-07-04 16:05:03 +0400

    fix congigurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as redisStore from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { configService } from './config/config.service';
 import { RolesModule } from './modules/security/roles/roles.module';
 import { ActivitiesModule } from './modules/security/activities/activities.module';
 import { UsersModule } from './modules/security/users/users.module';
@@ -14,7 +16,33 @@ import { AuthModule } from './modules/security/auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get<string>('REDIS_HOST'),
+        port: configService.get<number>('REDIS_PORT'),
+        auth_pass: configService.get<string>('REDIS_PASSWORD'),
+        ttl: configService.get<number>('CACHE_TTL', 3600),
+      }),
+      inject: [ConfigService],
+    }),
     UsersModule,
     CalendarModule,
     NotificationsModule,
