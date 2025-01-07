411745dd80c22dd24eb23808487ecdfdd46e28b5
Odai Nasser <karajah90@icloud.com>
2025-01-07 21:44:24 +0300

    create and update user dto

--- src/modules/security/users/dtos/create-user.dto.ts
+++ src/modules/security/users/dtos/create-user.dto.ts
@@ -1,7 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @ApiProperty({ description: 'The username of the user' })
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @ApiProperty({ description: 'The email of the user' })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @ApiProperty({ description: 'The password of the user' })
+  @IsString()
+  @MinLength(6)
+  @IsNotEmpty()
   password: string;
+
+  @ApiProperty({ description: 'The first name of the user', required: false })
+  @IsString()
+  @IsOptional()
   firstName?: string;
+
+  @ApiProperty({ description: 'The last name of the user', required: false })
+  @IsString()
+  @IsOptional()
   lastName?: string;
 }
