59113750c305e9a6ea121b3529ec03fd00d56371
Odai Nasser <karajah90@icloud.com>
2025-01-10 20:52:27 +0300

    update password validation fixing

--- src/modules/security/users/users.controller.ts
+++ src/modules/security/users/users.controller.ts
@@ -46,6 +46,9 @@ export class UsersController {
     if (password !== confirmPassword) {
       throw new Error('Passwords do not match');
     }
+    if (password.length < 8 || password.length > 20) {
+      throw new Error('Password must be between 8 and 20 characters');
+    }
     return this.usersService.updatePassword(id, password);
   }
 
